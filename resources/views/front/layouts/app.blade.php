dc5de3c30de9fe49ff65d869785cc87e103c6a9e
patelaman98 <aman.vns98@gmail.com>
2021-11-17 10:00:34 +0530

    first

---
+++ resources/views/front/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('front.layouts.head')
+<body class="animsition">
+
+    @include('front.layouts.nav')
+    @yield('content')
+    @include('front.layouts.footer')
+    @include('front.layouts.script')
+    
+</body>
+</html>l
